6aa4229008c1caf8ce9f421e4c9df5d9c68506ad
Rami Sayar <sayar@users.noreply.github.com>
2019-07-30 16:24:26 -0400

    Refactored and renamed in prep for @cnadolny's PR. (#7)
    * Refactored and renamed in prep for @cnadolny's PR.
    * Moved client helpers to a different folder to distinguish them from local tests scripts.

--- test/client_helpers/common.sh
+++ test/client_helpers/common.sh
@@ -35,31 +35,3 @@ function create_keystore {
     fi
     RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$3" -inkey "$4" -name "$HOSTNAME" -password "pass:$2" -out "$1"
 }
-
-if [ "$CA_CRT" ];
-then
-    echo "Preparing truststore"
-    TRUSTSTORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-    echo "$CA_CRT" > /tmp/ca.crt
-    create_truststore /opt/kafka/truststore.p12 "$TRUSTSTORE_PASSWORD" /tmp/ca.crt ca
-fi
-
-if [[ "$USER_CRT" && "$USER_KEY" ]];
-then
-    echo "Preparing keystore"
-    KEYSTORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-    echo "$USER_CRT" > /tmp/user.crt
-    echo "$USER_KEY" > /tmp/user.key
-    create_keystore /opt/kafka/keystore.p12 "$KEYSTORE_PASSWORD" /tmp/user.crt /tmp/user.key /tmp/ca.crt "$HOSTNAME"
-fi
-
-cat << EOF > /opt/kafka/config/ssl-config.properties
-security.protocol=SSL
-ssl.truststore.location=/opt/kafka/truststore.p12
-ssl.truststore.password=$TRUSTSTORE_PASSWORD
-ssl.truststore.type=PKCS12
-ssl.keystore.location=/opt/kafka/keystore.p12
-ssl.keystore.password=$KEYSTORE_PASSWORD
-ssl.keystore.type=PKCS12
-ssl.key.password=$KEYSTORE_PASSWORD
-EOF
--- test/perftest.sh
+++ test/perftest.sh
@@ -31,8 +31,9 @@ sleep 5s
 setup_kafka_client_ssl () {
   echo "Setting Up Kafka Client for SSL"
   for i in $(seq 0 2); do # End Number is replication factor of kafka client - 1
-    kubectl cp ./setup_ssl.sh "kafka/kafkaclient-$i:/opt/kafka/setup_ssl.sh"
-    kubectl exec -n kafka -it "kafkaclient-$i" -- bash setup_ssl.sh
+    kubectl cp ./client_helpers/common.sh "kafka/kafkaclient-$i:/opt/kafka/"
+    kubectl cp ./client_helpers/perftest_ssl.sh "kafka/kafkaclient-$i:/opt/kafka/"
+    kubectl exec -n kafka -it "kafkaclient-$i" -- bash perftest_ssl.sh
   done
 }
 
